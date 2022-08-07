fd6dfc62538f3472285ad3a0e187bfb9ac53f51f
bekaUser <itsbektur@gmail.com>
2022-08-08 02:12:20 +0600

    data models and relationships between them have been added

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 const express = require('express')
 const sequelize = require('./db')
+const models = require('./models/models')
+const cors = require('cors')
 
 const PORT = process.env.PORT || 5000
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
+app.get('/',(req,res) => {
+    res.status(200).json({message:"Working!!"})
+})
 
 const start = async ()=>{
     try{
