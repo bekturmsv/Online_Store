314f03409bed1519361ae92e35fd7f85c0afbb1d
bekaUser <itsbektur@gmail.com>
2022-08-08 17:55:47 +0600

    add receiving devices, filtering, pagination - pagination

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,6 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class DeviceController{
@@ -11,6 +11,17 @@ class DeviceController{
         let fileName = uuid.v4() + ".jpg"
          img.mv(path.resolve(__dirname,"..",'static',fileName))
 
+         if(info){
+            info = JSON.parse(info)
+            info.forEach(i => {
+                DeviceInfo.create({
+                    title: i.title,
+                    description: i.description,
+                    deviceId: device.id
+                })
+            })
+         }
+
          const device = await Device.create({name,price, brandId,typeId, img:fileName})
 
          return res.json(device)}
@@ -19,10 +30,34 @@ class DeviceController{
          }
     }
     async getAll(req, res){
-        
+        let {brandId, typeId,limit,page} = req.query
+        page = page || 1
+        limit = limit || 9
+        let offset = page * limit - limit
+        let devices;
+        if(!brandId && !typeId){
+            devices = await Device.findAndCountAll({limit,offset})
+        }
+        if(brandId && !typeId){
+            devices = await Device.findAndCountAll({where:{brandId},limit,offset})
+        }
+        if(!brandId && typeId){
+            devices = await Device.findAndCountAll({where:{typeId},limit,offset})
+
+        }
+        if(brandId && typeId){
+            devices = await Device.findAndCountAll({where:{brandId, typeId },limit,offset})
+
+        }
+        return res.json(devices)
     } 
     async getOne(req, res){
-        
+        const {id} = req.params
+        const device = await Device.findOne({
+            where: {id},
+            include:[{model:DeviceInfo, as: "info"}]
+        })
+        return res.json(device)
     } 
 }
 
--- server/models/models.js
+++ server/models/models.js
@@ -70,7 +70,7 @@ Rating.belongsTo(Device)
 Device.hasMany(BasketDevice)
 BasketDevice.belongsTo(Device)
 
-Device.hasMany(DeviceInfo)
+Device.hasMany(DeviceInfo, {as:"info"})
 DeviceInfo.belongsTo(Device)
 
 Type.belongsToMany(Brand, { through: TypeBrand })
